9e185e893de368e848978f9e77d9bf547c2b513d
aji wuryanto <aji.wuryanto@ui.ac.id>
2020-05-15 08:53:22 +0700

    return to int

--- src/main.cpp
+++ src/main.cpp
@@ -84,7 +84,7 @@ void writeOutClusters(string filePath, PointCloud<double> &pointData, std::vecto
 	fclose(fp);
 }
 
-void main(int argc, char **argv)
+int main()
 {
 	std::string fileData = "/home/aji/aji-skripsi/data/hasil-test/log1/dump/margonda_3d_Area_1_pred.txt";
 	std::string fileResult = "/home/aji/aji-skripsi/data/hasil-test/log1/dump/result.txt";
@@ -133,5 +133,4 @@ void main(int argc, char **argv)
 	
 	// step4: write out result
 	writeOutClusters(fileResult, pointData, clusters);
-	return 0;
 }
