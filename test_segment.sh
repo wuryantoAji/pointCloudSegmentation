ddc3ce6df64783f2520f5a9c9d95df0dfa4a0b0a
aji wuryanto <aji.wuryanto@ui.ac.id>
2020-05-17 09:30:29 +0700

    change script for docker

--- test_segment.sh
+++ test_segment.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
-
 # This script will build code and run the point cloud segmentation
 
-
-mkdir -p build && cd build
-
+mkdir -p ~/home/aji/aji-skripsi/pointCloudSegmentation/build && cd ~/home/aji/aji-skripsi/pointCloudSegmentation/build
 
 cmake ..
 make
